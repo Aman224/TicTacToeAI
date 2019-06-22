7cd595954011d4ced60f9da59d013db2c17e84c9
Aman224 <amanclct@gmail.com>
2019-06-23 00:57:31 +0530

    AI using Minimax Algorithm

--- js/script.js
+++ js/script.js
@@ -76,11 +76,12 @@ function gameOver(gameWon) {
 }
 
 function emptySquares() {
-    return board.filter(s => typeof s == 'number');
+     return board.filter(s => typeof s == 'number');
 }
 
+
 function idealSpot() {
-    return emptySquares()[0];
+    return minimax(board, aiPlayer).index;
 }
 
 function declareWinner(who) {
@@ -100,3 +101,55 @@ function checkTie() {
     return false;
 }
 
+function minimax(newBoard, player) {
+    var availSpots = emptySquares();
+
+    if (checkWin(newBoard, huPlayer)) {   // Player wins
+        return {score: -10};
+    } else if (checkWin(newBoard, aiPlayer)) {  // AI Wins
+        return {score: 10};
+    } else if (availSpots.length === 0) {   // Draw
+        return {score: 0};
+    }
+
+    var moves = [];
+    for(var i = 0; i < availSpots.length; i++) {
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+        newBoard[availSpots[i]] = player;
+
+        if(player == aiPlayer) {
+            var result = minimax(newBoard, huPlayer);
+            move.score = result.score;
+        } else {
+            var result = minimax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+
+        newBoard[availSpots[i]] = move.index;
+
+        moves.push(move);
+    }
+
+    var bestMove;
+
+    if(player === aiPlayer) {
+        var bestScore = -10000;
+        for(var i = 0; i < moves.length; i++) {
+            if(moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        var bestScore = 10000;
+        for(var i = 0; i < moves.length; i++) {
+            if(moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+
+    return moves[bestMove];
+}
